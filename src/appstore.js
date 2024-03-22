b737081e23eeab65a2fd8b2b784d0ad83a6f31ce
yap cheah shen <yapcheahshen@gmail.com>
2024-03-22 22:31:50 +0800

    source and flag

---
+++ src/appstore.js
@@ -0,0 +1,18 @@
+export * from 'accelon23/src/store.js';
+import {ACC23 as _ACC23} from 'accelon23/src/appconst.js'
+
+_ACC23.CacheName='v1::anthem',
+_ACC23.AppVer='24.3.22';
+_ACC23.AppName='anthem'
+_ACC23.AppTitle='國家'
+_ACC23.allptks=['anthem','anthem-en'];
+_ACC23.selectedptks=['anthem','anthem-en']
+_ACC23.activeptk='anthem'
+_ACC23.ptkinfo={
+    'anthem':"中文版",
+    'anthem-en':"英文版"
+}
+_ACC23.favorites=[];
+_ACC23.MAXFAVORITE=100;
+_ACC23.ITEMPERPAGE=5;
+export const ACC23 = _ACC23;
